743a4a697800904431250123d3e93728583bf96a
HyeWon Shin <ggobugi@HyeWonui-MacBookPro.local>
2019-10-29 19:04:04 +0900

    2nd submit

--- 151.reverse-words-in-a-string.py
+++ 151.reverse-words-in-a-string.py
@@ -7,12 +7,18 @@
 # @lc code=start
 class Solution:
     def reverseWords(self, s: str) -> str:
-        new = ""
+        # new = ""
         split = s.split()
-        for letter in split:
-            new = letter + " " + new
-        new = new.rstrip()
-        return new
+        # for letter in split:
+        #     new = letter + " " + new
+        # new = new.rstrip()
+        # return new
+
+        split_list_reversed = split[::-1]
+        # print(split_list_reversed)
+        split_string = " ".join(split_list_reversed)
+        # print(split_string)
+        return split_string
 
         
 # @lc code=end
