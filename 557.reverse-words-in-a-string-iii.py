b5c7042f4c31b0ec4017d9fd1c92d09e58abb22e
HyeWon Shin <ggobugi@HyeWonui-MacBookPro.local>
2019-10-29 18:55:04 +0900

    2nd submit

--- 557.reverse-words-in-a-string-iii.py
+++ 557.reverse-words-in-a-string-iii.py
@@ -9,11 +9,18 @@ class Solution:
     def reverseWords(self, s: str) -> str:
         new = ""
         split = s.split()   # split the words
-        for letter in split:  # traverse each word in words
-            new = letter + " " + new
-        new = new.rstrip()
-        print(new)
-        return "".join(new[::-1]) 
-               
+        split_list_reversed = split[::-1]
+        # print(split_list_reversed)
+        split_string = " ".join(split_list_reversed)
+        # print(split_string)
+        return split_string[::-1]
+
+        
+        # for letter in split:  # traverse each word in words
+        #     new = letter + " " + new
+        # new = new.rstrip()
+        # print(new)
+        # return "".join(new[::-1]) 
+
 # @lc code=end
 
