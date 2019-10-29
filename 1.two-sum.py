12f331bb25294c13177ea00893f27f0908bfa955
HyeWon Shin <ggobugi@HyeWonui-MacBookPro.local>
2019-10-29 17:46:24 +0900

    first submit

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -7,6 +7,12 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        h = {}
+        for i, num in enumerate(nums):
+            n = target - num
+            if n not in h:
+                h[num] = i
+            else:
+                return [h[n], i]        
 # @lc code=end
 
