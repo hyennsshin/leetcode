b9f1326812d243d5b1e6e98872a7e7b044e9bfb7
HyeWon Shin <ggobugi@HyeWonui-MacBookPro.local>
2019-10-29 17:56:21 +0900

    2nd submit

--- 27.remove-element.py
+++ 27.remove-element.py
@@ -7,15 +7,18 @@
 # @lc code=start
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        count = 0
-        if len(nums) == 0:
-            return 0
-        for i in range(len(nums)):
-            if nums[i] != val:
-                nums[count] = nums[i]
-                count += 1
-        
-        return count
-                
+#         count = 0
+#         if len(nums) == 0:
+#             return 0
+#         for i in range(len(nums)):
+#             if nums[i] != val:
+#                 nums[count] = nums[i]
+#                 count += 1
+#         return count
+    
+        while val in nums:
+            nums.remove(val)
+        return len(nums)
+
 # @lc code=end
 
