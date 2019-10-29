2dd26f09e4d0b81728270118ee7eb6f9631118ba
HyeWon Shin <ggobugi@HyeWonui-MacBookPro.local>
2019-10-29 17:53:17 +0900

    1st submit

---
+++ 26.remove-duplicates-from-sorted-array.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=26 lang=python3
+#
+# [26] Remove Duplicates from Sorted Array
+#
+
+# @lc code=start
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        count = 1
+        if len(nums) == 0:
+            return 0
+        for i in range(1,len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[count] = nums[i]
+                count += 1
+            
+        return count
+                
+# @lc code=end
+
---
+++ 27.remove-element.py
@@ -0,0 +1,21 @@
+#
+# @lc app=leetcode id=27 lang=python3
+#
+# [27] Remove Element
+#
+
+# @lc code=start
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        count = 0
+        if len(nums) == 0:
+            return 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[count] = nums[i]
+                count += 1
+        
+        return count
+                
+# @lc code=end
+
