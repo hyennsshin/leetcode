f879b8ba29a277185130fe4eca8447f23efd49d8
HyeWon Shin <ggobugi@HyeWonui-MacBookPro.local>
2019-10-29 17:42:21 +0900

    my second commit

--- 13.roman-to-integer.py
+++ 13.roman-to-integer.py
@@ -6,8 +6,18 @@
 
 # @lc code=start
 class Solution:
-    def romanToInt(self, s: str) -> int:
-        return
+    def romanToInt(self, s: str) -> int:       
+        rom_to_int = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
+        val = 0
+        pre_val = 0
+        answer = 0
+        for letter in s:
+            val = rom_to_int[letter]
+            answer += val
+            if val > pre_val:
+                answer -= pre_val*2
+            pre_val = val   
+        return answer
         
 # @lc code=end
 
